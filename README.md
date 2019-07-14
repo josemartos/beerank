09e74ab42a1da0b5886aa0ada10bfe53897ec4a4
Jose <martosjosele@gmail.com>
2019-07-14 11:57:05 +0200

    Add React and Parcel

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# petproject" 
+# petproject
+
+Simple React application
+
+## description
+
+Personal pet-project used as an object of experimentation to test new technologies, libraries, frameworks, techniques, etc.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            Hello my new friend
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
