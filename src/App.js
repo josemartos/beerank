593b18888bb03b448286a8724627599ea99a0dce
Jose <martosjosele@gmail.com>
2019-07-19 22:01:46 +0200

    Adding simple code splitting for fun

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { Router } from "@reach/router";
+import Spinner from "./components/Spinner";
 import Header from "./components/Header";
 import UserList from "./components/UserList";
-import UserDetails from "./components/UserDetails";
+
+const UserDetails = lazy(() => import("./components/UserDetails"));
 
 const App = () => {
   return (
     <div>
       <Header />
       <main>
-        <div className="pageContainer">
-          <Router>
-            <UserList path="/" />
-            <UserDetails path="/userdetails/:id" />
-          </Router>
-        </div>
+        <Suspense fallback={<Spinner />}>
+          <div className="pageContainer">
+            <Router>
+              <UserList path="/" />
+              <UserDetails path="/userdetails/:id" />
+            </Router>
+          </div>
+        </Suspense>
       </main>
     </div>
   );
